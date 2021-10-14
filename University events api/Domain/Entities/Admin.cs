1db9e955c4573cb7ee1caa40f1a88f3f69e50aa5
IvanVynyavsky <ivanvynyavsky@gmail.com>
2021-10-14 18:13:21 +0300

    Create entities Done

---
+++ University events api/Domain/Entities/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Admin : IEntity
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ University events api/Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Person : IEntity
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+
+        public int ImageId { get; set; }
+        public Image Image { get; set; }
+    }
+}
